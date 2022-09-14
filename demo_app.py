cfcd4ab9aa545437f1ab221c6373474e2caab210
Nathan <nat272@gmail.com>
2022-09-14 10:35:45 +0200

    Improve Demo app

--- demo_app.py
+++ demo_app.py
@@ -2,7 +2,26 @@ import streamlit as st
 
 from st_mui_multiselect import st_mui_multiselect
 
-st.subheader("Component:")
+st.subheader("Demo of Streamlit Material UI Multiselect Component")
+
 options = ["Mayo", "Lettuce", "Pickles", "Tomatoes", "Onions", "Mushrooms", "Ketchup", "Jalapeños"]
-selections = st_mui_multiselect(options, size=10)
+select_size = st.slider(
+    "size",
+    1,
+    10,
+    4,
+)
+selections = st_mui_multiselect(options, size=select_size)
 st.markdown("You selected %s" % ", ".join(selections))
+
+with st.form("try_a_form"):
+    sel_size = st.slider(
+        "size",
+        1,
+        10,
+        4,
+    )
+    sel = st_mui_multiselect(options, size=sel_size, key="second")
+    sub = st.form_submit_button("Submit")
+    if sub:
+        st.write("You selected %s" % ", ".join(sel))
